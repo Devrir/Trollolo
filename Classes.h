2a71c4f5be5e7650ae202ee1f6bc95f082fe4c03
Devrir <koksuvi@gmail.com>
2017-02-13 09:53:54 +0100

    Update Classes.h

--- Classes.h
+++ Classes.h
@@ -1,33 +1,47 @@
 #ifndef CLASSES_H
 #define CLASSES_H
 
+#include<iostream>
 #define WALL 2
 #define BLOCK 1
 #define AIR 0
 
 #define HEIGHT 20
 #define WIDTH 15
+
 class Frame{
 	int FrameTab[WIDTH][HEIGHT];
 	int Box[WIDTH][HEIGHT];
 	public:
 	void InitBox();
+	void debug_OutBox();
 	void FrameMakeSum();
 };
 void Frame::InitBox(){
 	for(int i=0;i<HEIGHT;i++){
+		
 		if(i==HEIGHT-1){
 			for(int j=0;j<WIDTH;j++){
-				Box[j][i]=WALL;
+			    Box[j][i]=WALL;
 			}
 		}
+		
 		else {
-		Box[0][i]=WALL;
-		for(int j=1;j<WIDTH-1;j++){
-			Box[j][i]=AIR;
-		}
-		Box[WIDTH][i]=WALL;
+		        Box[0][i]=WALL;
+		    for(int j=1;j<WIDTH;j++){
+			    Box[j][i]=AIR;
+	      	}
+		    Box[WIDTH-1][i]=WALL;
+	    }
 	}
+}
+
+void Frame::debug_OutBox(){
+	for(int i=0;i<HEIGHT;i++){
+		for(int j=0;j<WIDTH;j++){
+			std::cout<<Box[j][i];
+		}
+		std::cout<< "\n";
 	}
 }
 
